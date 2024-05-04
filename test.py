b8d97da1b39e9e4c91c798f6178dc91f278b89c7
Nasir Dastgirzada <nasirdastgirzada@gmail.com>
2024-05-05 00:54:14 +0200

    updated
    answers of the Exercises _5 added.

--- test.py
+++ test.py
@@ -1,8 +1,2 @@
-lst_1 = ["one", "four", "nine"]
-lst_2 = ["*", "@", "False"]
-lst_3 = [True , False]
-lst_4 = [[3],[44], [-12] ]
-lst_5 = [[1,2], [44, -40], [44,-50], [-12, 1]]
-
-lst_1 = (len(lst_1))
-print(lst_1)
+mytuple = tuple("apple")
+print(mytuple)
